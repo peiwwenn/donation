ed4d392a54a2735a6da1900e4595851f412e2259
eunice5214 <qeunicechan@gmail.com>
2026-02-05 14:22:56 +0800

    Remove exposed Firebase config

--- food-rescue/public/js/firebase.js
+++ food-rescue/public/js/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrw9JfpfWOQSaDGHSHqQ9twIT8w3uXNTw",
+  apiKey: "GOOGLE_MAPS_API_KEY",
   authDomain: "smart-food-rescue-v2.firebaseapp.com",
   projectId: "smart-food-rescue-v2",
   storageBucket: "smart-food-rescue-v2.firebasestorage.app",
